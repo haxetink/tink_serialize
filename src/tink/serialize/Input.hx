878ff8169454b28185ea6c7270bc5b85fdfd69e0
back2dos <back2dos@gmail.com>
2013-08-23 21:40:00 +0200

    Added possibility to specify leading byte (used to deal with Null<Int>). Added float API.

--- src/tink/serialize/Input.hx
+++ src/tink/serialize/Input.hx
@@ -2,8 +2,10 @@ package tink.serialize;
 
 abstract Input<T:haxe.io.Input>(T) from T to T {
 	public function readByte() return this.readByte();
-	public function readLength():Int {
-		var b = readByte();
+	public function readLength(?first:Null<Int> = null):Int {
+		var b = 
+			if (first == null) readByte();
+			else first;
 		return 
 			if (b < 0x80) b;
 			else if (b < 0xC0) ((b & 0x3F) << 8) + readByte();
@@ -11,6 +13,8 @@ abstract Input<T:haxe.io.Input>(T) from T to T {
 			else if (b < 0xF0) ((b & 0x0F) << 24) + (readByte() << 16) + (readByte() << 8) + readByte();
 			else (readByte() << 24) + (readByte() << 16) + (readByte() << 8) + readByte();
 	}
+	public function readFloat() return this.readFloat();
+	public function readDouble() return this.readDouble();
 	public function readString():String {
 		return StringTools.urlDecode(this.readString(readLength()));
 	}
